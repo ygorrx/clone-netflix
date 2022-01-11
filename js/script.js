420bea8e549304b9f95faa97419f15765b48c2a5
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-01-11 20:09:48 -0300

    mudanças finais + responsividade

--- js/script.js
+++ js/script.js
@@ -15,3 +15,15 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+let show = true; 
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
